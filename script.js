9e751dd3c21d77463d6696e9db143fdbca7591d6
Merica <142843663+mericadev@users.noreply.github.com>
2024-02-29 12:09:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSidebar () {
+    const sidebar = document.querySelector('.side-bar');
+    sidebar.style.display = 'flex';
+}
+
+function closeSidebar () {
+    const sidebar = document.querySelector('.side-bar');
+    sidebar.style.display = 'none';
+}
